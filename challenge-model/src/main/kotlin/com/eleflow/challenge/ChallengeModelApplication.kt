f019c9b559ccf08f504336cfe16c777204b6d459
Lucas Sanches Fantacuci <lucas.fantacuci@sciensa.com>
2020-11-24 08:57:35 -0300

    [CE-01] - Inicializando repositório

---
+++ challenge-model/src/main/kotlin/com/eleflow/challenge/ChallengeModelApplication.kt
@@ -0,0 +1,11 @@
+package com.eleflow.challenge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChallengeModelApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChallengeModelApplication>(*args)
+}
---
+++ challenge-model/src/test/kotlin/com/eleflow/challenge/ChallengeModelApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.eleflow.challenge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChallengeModelApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
